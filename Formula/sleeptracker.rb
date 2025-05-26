fe5a0886eb13539385a5e1ebc7fd56c94d6c1c14
LimDonghyun <burntires@naver.com>
2025-05-26 14:13:47 +0900

    Add files via upload

---
+++ Formula/sleeptracker.rb
@@ -0,0 +1,33 @@
+class Sleeptracker < Formula
+  desc "SRFF 기반 macOS 수면 리듬 감지 유틸리티"
+  homepage "https://github.com/LimDonghyun/sleeptracker"
+  url "https://github.com/LimDonghyun/sleeptracker/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "164098911e07d5e0fce97c805ec897d49902d2d90f123188a458320a476389b4"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    system "python3", "-m", "venv", "venv"
+    system "./venv/bin/pip", "install", "-r", "requirements.txt"
+
+    bin.install "sleep_tracker_cli/tracker.py" => "sleeptracker"
+    prefix.install "xyz.extho.sleeptracker.mac.wakeup.plist"
+  end
+
+  def caveats
+    <<~EOS
+      📦 SleepTracker 설치 완료
+
+      ▶ launchd 등록:
+         cp #{opt_prefix}/xyz.extho.sleeptracker.mac.wakeup.plist ~/Library/LaunchAgents/
+         launchctl load -w ~/Library/LaunchAgents/xyz.extho.sleeptracker.mac.wakeup.plist
+
+      ▶ 실행:
+         sleeptracker
+
+      ▶ 설치 위치:
+         #{opt_prefix}
+    EOS
+  end
+end
