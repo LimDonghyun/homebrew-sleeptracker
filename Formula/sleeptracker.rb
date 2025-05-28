5a288bc0169c3906b78767b1db12469addef9c71
LimDonghyun <burntires@naver.com>
2025-05-28 13:39:45 +0900

    Update sleeptracker.rb

--- Formula/sleeptracker.rb
+++ Formula/sleeptracker.rb
@@ -2,7 +2,7 @@ class Sleeptracker < Formula
   desc "SRFF 기반 macOS 수면 리듬 감지 유틸리티"
   homepage "https://github.com/LimDonghyun/sleeptracker"
   url "https://github.com/LimDonghyun/sleeptracker/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "164098911e07d5e0fce97c805ec897d49902d2d90f123188a458320a476389b4"  # ← 이걸로 교체
+  sha256 "49d5a73259564e7bcd6c7e739e4f31ffabcc7f7a0c73d565e65cad57028d5736"
   version "0.1.1"
   license "MIT"
 
